584c16ecf01396b03c69375f91d5511bce0d0d99
HAO <473164786@qq.com>
2018-06-25 14:03:07 +0800

    do some demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+
+#[derive(Debug)]
+enum Per {
+    name(String),
+    types,
+    value,
+}
+#[derive(Debug)]
+struct User {
+    name: String,
+    age: u32,
+    job: String,
+}
+
+fn show_something(persion: &User) {
+    println!("{}",persion.name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let demo = Per::name(String::from("var"));
+    let joe = User {
+    name: String::from("joe"),
+    age: 22,
+    job: String::from("programmer")
+    };
+    show_something(&joe)
 }
